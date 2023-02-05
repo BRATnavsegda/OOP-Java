454441ad31295439237ffd1e10316736eb6b16e0
BRATnavsegda <109758944+BRATnavsegda@users.noreply.github.com>
2023-02-05 13:59:22 +0300

    hw5 update

--- HomeWork5/src/main/java/org/phonebook/MyLogger.java
+++ HomeWork5/src/main/java/org/phonebook/MyLogger.java
@@ -1,14 +1,16 @@
 package org.phonebook;
 
+import java.io.FileInputStream;
 import java.util.logging.*;
 
 public class MyLogger {
 
     public static Logger logger = Logger.getGlobal();
     static {
-        try {
-            LogManager.getLogManager().readConfiguration(Main.class.getResourceAsStream("log.config"));
-        } catch (Exception ignore) {
+        try(FileInputStream ins = new FileInputStream("C:\\Users\\TESO\\Desktop\\Lessions\\ООП Java\\HomeWork5\\src\\main\\java\\org\\phonebook\\log.config")){
+            LogManager.getLogManager().readConfiguration(ins);
+            logger = Logger.getLogger(Main.class.getName());
+        }catch (Exception ignore){
             ignore.printStackTrace();
         }
     }
